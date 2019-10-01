ac133541ea0e8cc3768f313c869af32f4552e2c0
alejandro romero <aleja@DESKTOP-U2Q2U28>
2019-10-01 19:39:21 +0200

    Primeros desvaries

---
+++ ClassInheritExercise/Campesino.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public interface ICampesino
+{
+    int hp;
+    int mana;
+    int exp;
+	public ICampesino( int hp,int mana,int exp)
+	{
+        this.hp = hp;
+        this.mana = mana;
+        this.exp = exp;
+	}
+}
+public class Mago : ICampesino
+{
+    public Mago(int hp, int mana, int exp) : base(hp, mana, exp){
+        hp = hp;
+        mana = mana+100;
+        exp = exp;
+    }
+
+}
+public class Guerrero : ICampesino
+{
+    public Guerrero(int hp, int mana, int exp) : base(hp, mana, exp)
+    {
+        hp = hp+100;
+        mana = mana;
+        exp = exp;
+    }
+}
