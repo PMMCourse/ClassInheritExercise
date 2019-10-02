58d2cb57f4191f083f9c53e130a3e56390ce2872
Carlos Vasili Viñuela Garcia <37907899+cvasilivg@users.noreply.github.com>
2019-10-02 21:44:46 +0200

    Update ArchBishop.cs
    Updated comment

--- ClassInheritExercise/ArchBishop.cs
+++ ClassInheritExercise/ArchBishop.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassInheritExercise
 {
-    // Interfaz Curador
+    // Interfaz ArzoBispo
     interface ArchBishop
     {
         // Metodo Lanzar Lluvia Celestial
