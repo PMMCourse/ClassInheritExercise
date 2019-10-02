18d5322e6c8ae1971c084d827f6d48718d1cf9b7
Antonio Morón Pozo <sevillista-97@hotmail.com>
2019-10-02 21:47:01 +0200

    Se crean Las clases Campesiono Mago etc y la Interfaz Ihechizos

---
+++ ClassInheritExercise/Picaro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInheritExercise
+{
+    class Picaro : Campesino
+    {
+        //Atributos
+
+        int evasion { get; protected set; }
+        int velocidad { get; protected set; }
+
+        //Contructor
+
+        public Picaro (int e,int v) : base(HP, MP, EXP, n, nivel)
+        {
+            this.evasion = e;
+            this.velocidad = v;
+
+        }
+
+        //Metodos
+
+        private void Robar();
+        private void Invisible();
+    }
+}
---
+++ ClassInheritExercise/Sacedorte.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInheritExercise
+{
+    class Sacedorte : Campesino, Ihechizos
+
+    {
+        //Atributos
+
+        int espiritu;
+
+        //Constructor
+        public Sacerdote (int esp) : base (HP, MP,  EXP,n,nivel)
+        {
+            this.espiritu = esp;
+        }
+
+        //Metodos
+
+        private void Curar();
+        private void Revivir();
+    }
+}
