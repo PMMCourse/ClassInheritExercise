739df1c710c2da8b157b4299cdb91e3e9cdf74d3
joaquinBB99 <joaquin.bejarano.brenes@alumnos.fesac.es>
2019-10-03 09:00:23 +0200

    Changes created

---
+++ ClassInheritExercise/FusilesPrecision.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInheritExercise
+{
+   public  class FusilesPrecision : Armas, ISecundarias
+    {
+        public string Fusilp;
+
+        FusilesPrecision(string fusilp, double cadencia, double retroceso, double distancia, int daño, int manejabilidad) : base(cadencia, retroceso, distancia, daño, manejabilidad)
+        {
+            //vacio
+        }
+        public static void FusilesPrecisionBo2()
+        {
+            FusilesPrecision fp = new FusilesPrecision("DSR-50", 3.56, 17.67, 19.67, 10, 9);
+
+            //Console.WriteLine("Arma= " + fp.Fusilp, fp.cadencia, fp.retroceso, fp.distancia, fp.daño, fp.manejabilidad);
+        }
+
+        public void IntroduccionDeArmasSecundarias()
+        {
+            //Con un francotirador siempre es importante llevar una secundaria, para el cuerpo a cuerpo
+        }
+    }
+}
