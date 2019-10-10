cfb1483422b4a1e3a88e5fb0e9c3fff255a84c73
Fran <frasiecue@gmail.com>
2019-10-10 16:29:30 +0200

    ClasesMMO

---
+++ ClassInheritExercise/Aventurero.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInheritExercise {
+    //Esta interfaz está metido un poco obligada, la idea
+    //es crear un guerrero que pueda obtener los métodos
+    //de un aventurero. Básicamente por darle uso.
+    interface Aventurero {
+        void CorrerRapido();
+        void DescubrirObjeto();
+    }
+}
---
+++ ClassInheritExercise/Party.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInheritExercise {
+    //He mirado un poco por Internet
+    //el rollo Party de los juegos de MMO
+    //y no encuentro unas propiedades fijas.
+    //Me hago la idea de que tendrán un server y un límite de usuarios.
+    class Party {
+        private string nombre;
+        private int maxJugadores;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int MaxJugadores { get => maxJugadores; set => maxJugadores = value; }
+    }
+}
